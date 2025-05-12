b7b7036e813ae47169dee94f135b11579bc42b54
Abass Durodola <abass_gbolahan@yahoo.com>
2025-05-12 10:27:59 +0100

    initial & setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// most errors in  JS occurs at runtime but in TS, errors happens in compile time
+// npm install -g typescript
+// npm init -y ||
+// npm install typescript --save-dev
+// npx tsc --init : creating typescript configuration file
+
+// write the typescript code and compile with tsc command
