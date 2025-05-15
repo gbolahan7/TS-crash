25175de022640f701fdd5687f986068cd705cc93
Abass Durodola <abass_gbolahan@yahoo.com>
2025-05-15 11:27:47 +0100

    arrays & tuples

---
+++ READMe.md
@@ -0,0 +1,6 @@
+## setting up basic ts
+
+### run npm init - y
+### nom install typescript --save-dev
+### npx tsc --init
+### create ts file (app.ts)
--- app.ts
+++ app.ts
@@ -12,3 +12,25 @@ z = true;
 
 let result = null; // we don't expect this to have any value i.e explicitly declared something to be empty
 
+
+
+// ARRAYS
+
+let arr: number[] = [1, 2, 3, 4, 5]; // array of numbers
+let arr2: Array<string> = ["a", "b", "c"]; // array of strings
+let arr3: (number | string)[] = [1, "a", 2, "b"]; // array of numbers and strings
+let arr4: Array<number | string> = [1, "a", 2, "b"]; // array of numbers and strings
+
+// TUPLES: fixed-length arrays that can contain different types
+
+let tuple: [number, string] = [1, "a"]; // tuple of number and string
+let tuple2: [number, string, boolean] = [1, "a", true]; // tuple of number, string and boolean
+let tuple3: [number, string, boolean?] = [1, "a"]; // optional boolean
+
+const coords: [number, number][] = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+]; // tuple of two numbers
+coords[0][0] = 10; // change the first number of the first tuple
+coords[0][1] = 20; // change the second number of the first tuple
